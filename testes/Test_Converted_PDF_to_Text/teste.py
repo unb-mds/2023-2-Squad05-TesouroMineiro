9f93b497534144b4869c4519a7a041e92efd86f8
elias <eliasoliveiradapaz@hotmail.com>
2023-10-23 14:49:29 -0300

    testes

--- testes/Test_Converted_PDF_to_Text/teste.py
+++ testes/Test_Converted_PDF_to_Text/teste.py
@@ -1,6 +1,7 @@
 from playwright.sync_api import sync_playwright
 import time
 from PyPDF2 import PdfReader
+import tabula
 
 def automacao_web_DOUSs():
     with sync_playwright() as p:
@@ -12,7 +13,7 @@ def automacao_web_DOUSs():
         pagina.goto("https://www.diariomunicipal.com.br/amm-mg/")
         #pagina.screenshot(path="teste.png")
 
-        pagina.locator('//*[@id="formCalendario"]/fieldset/div/div[2]/table/tbody/tr[5]/td[4]/a').click()
+        pagina.locator('//*[@id="formCalendario"]/fieldset/div/div[2]/table/tbody/tr[1]/td[3]/a').click()
 
         #pagina.locator('//*[@id="btDownloadSimples2"]').click()
 
@@ -20,7 +21,9 @@ def automacao_web_DOUSs():
         with pagina.expect_download() as download_info:
             # Perform the action that initiates download
             pagina.click('//*[@id="btDownloadSimples2"]')
+            global download
         download = download_info.value
+        
 
         # Wait for the download process to complete and save the downloaded file somewhere
         download.save_as(download.suggested_filename)
@@ -30,16 +33,15 @@ def automacao_web_DOUSs():
     time.sleep(1000)
 
 
-#pdf = str('publicado_96007_2023-09-26_2b094e14c97153e332a4be1408b108cc.pdf')
 
 def catch_pdf():
         
     #Abrindo aqruivo pdf em modo leitura e lendo em binario
-    reader = PdfReader(open('publicado_96007_2023-09-26_2b094e14c97153e332a4be1408b108cc.pdf', 'rb'))
+    reader = PdfReader(open("publicado_96011_2023-10-02_f8411b1cd47b4150f7026ca15e88202f.pdf", 'rb'))
     #Mostrando número de páginas do arquivo
     print(len(reader.pages))
     #Armazenando uma pagina na variavel page
-    page = reader.pages[50]
+    page = reader.pages[308]
     #Extraindo o texto da pagina
     text = page.extract_text()
 
@@ -49,10 +51,23 @@ def catch_pdf():
         print("Erro ao ler o arquivo pdf erro {}".format(e))
 
     
-    with open('DOUMG.txt', 'w') as f:
+    with open('/home/elias/Unb/2023-2-Squad05/testes/Test_Converted_PDF_to_Text/publicado_96007_2023-09-26_2b094e14c97153e332a4be1408b108cc.pdf', 'w') as f:
             f.write(text)
             f.close()
     
 #automacao_web_DOUSs()
-catch_pdf()
+#catch_pdf()
 
+
+def tabela():
+     
+        # Substitua "seu_arquivo.pdf" pelo caminho do seu arquivo PDF
+    arquivo_pdf = "Test_Converted_PDF_to_Text/arquivo_doy_mg.pdf"
+
+    # Extrai as tabelas do PDF e retorna uma lista de DataFrames do pandas
+    tabelas = tabula.read_pdf(arquivo_pdf, pages='307')
+
+    print(tabelas)
+
+
+tabela()
