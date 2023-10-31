9822cd336485e574a7fd6ce7fa664fce41b37587
elias <eliasoliveiradapaz@hotmail.com>
2023-10-31 20:19:10 -0300

    frontend com sidebar

---
+++ frontend/Pages/Dados.js
@@ -0,0 +1,14 @@
+const Dados = () => {
+
+    return (
+
+        <div>
+        
+        <h1>Dados
+        </h1>
+
+    </div>
+    )
+}
+
+export default Dados;
---
+++ frontend/Pages/Sobre.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Sobre = () => {
+    
+    return(
+    
+    <div>
+        
+        <h1>Sobre
+        </h1>
+
+    </div>
+
+    )
+}
+
+export default Sobre;
