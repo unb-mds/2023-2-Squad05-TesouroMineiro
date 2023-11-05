7d48bc845ff05d8b84092546a1428351a78d2614
CLaudio Henrique <claudio885henrique@gmail.com>
2023-11-05 18:07:52 -0300

    fix: erro de nomes de municipio

--- busca-keywords/trechos_municipios.py
+++ busca-keywords/trechos_municipios.py
@@ -5,7 +5,7 @@ import re
 pasta = 'diarios_spiders/diarios/full'
 
 # Palavra-chave a ser pesquisada
-keyword = 'gratificação'
+keyword = 'NOMEIA'
 
 if os.path.isdir('busca-keywords/trechos'): # verica se o diretorio ja existe
     print ('Ja existe a pasta "trechos"!')
@@ -34,16 +34,24 @@ for nome_arquivo in os.listdir(pasta):
             bloco = bloco.strip()
             
             # Nome do Municipio - alterar busca de nomes*******
-            padraoMunicipio = r'DE\s*([A-ZÁÀÃÉÈÍÏÓÒÕÚÇ ]+)(?:\s+([A-ZÁÀÃÉÈÍÏÓÒÕÚÇ ]+))?'
-            municipio = re.search(padraoMunicipio, bloco)
-            nomeDoMunicipio = municipio.group(1).strip()
-            try:
-                segundaPalavra = municipio.group(2).strip()
-                if "SECRETARIA" not in segundaPalavra and "ASSESSORIA" not in segundaPalavra and "GABINETE" not in segundaPalavra and "CONSULTORIA" not in segundaPalavra:
-                    nomeDoMunicipio += " " + segundaPalavra
-            except:
-                pass
+            re_nomes_municipios = (r"DE\s*([A-ZÁÂÀÃÉÈÍÏÓÔÒÕÚÇ ]+)(?:\s+([A-ZÁÂÀÃÉÈÍÏÓÔÒÕÚÇ ]+))?")
+            #nomes_municipios = re.findall(re_nomes_municipios, bloco)
+            match = re.search(re_nomes_municipios, bloco, re.MULTILINE)
+            print(match)
+            if match:
+                nomeDoMunicipio = match.group(1)
+                nomeDividido = nomeDoMunicipio.split()
+                if len(nomeDividido) > 4:
+                    nomeDoMunicipio =  ' '.join(nomeDividido[0:4])
+                
+                print("Nome do município:", nomeDoMunicipio)
 
+            else:
+                print("Nenhum município correspondente encontrado.")
+                
+           
+
+    
 
             # Usa regex para encontrar todas as ocorrências da palavra-chave
             ocorrencias = re.finditer(fr'\b{re.escape(keyword)}\b', bloco, re.IGNORECASE)
@@ -53,7 +61,7 @@ for nome_arquivo in os.listdir(pasta):
 
             # Cria um arquivo exclusivo para cada arquivo de origem
             nome_arquivo_base = os.path.splitext(nome_arquivo)[0]  # Remove a extensão .txt
-            arquivo_destino = f'{nome_arquivo_base}_trechos-{nomeDoMunicipio}.txt'
+            arquivo_destino = f'{nomeDoMunicipio}-{nome_arquivo_base}_trechos.txt'
 
             # Salva os trechos no arquivo exclusivo
             if len(trechos) != 0:
