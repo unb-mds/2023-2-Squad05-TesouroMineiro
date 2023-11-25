e5eeeabd5ac7600f41b6177395474edc0e7211f0
Victor <vh_costa@hotmail.com>
2023-11-25 18:48:59 -0300

    Ajustes na navbar

--- frontend/src/Components/Navbar.js
+++ frontend/src/Components/Navbar.js
@@ -3,27 +3,39 @@ import logo from '../images/UnB.png';
 const Navbar = ({ show }) => {
 
     return (
-        <div style={{ width: '130px' }} className={show ? 'sidenav active' : 'sidenav'}>
-            <ul className='d-flex flex-column mx-0'>
-                <li>
-                    <img src={logo} alt="Logo" className='logo ms-3' />
-                </li>
+        <>
+            <nav className="navbar navbar-expand-lg w-100" style={{ backgroundColor: "#011627", height: '70px' }}>
+                <div class="w-100 d-flex mx-2">
+                    <a class="navbar-brand px-0" href="#"
+                    ><img
+                            id="MDB-logo"
+                            src={logo}
+                            alt="MDB Logo"
+                            draggable="false"
+                            height="30"
+                        /></a>
+                    <div className="container-fluid px-0">
+                        <div className="px-0" id="">
+                            <ul className="nav px-0 align-items-center mt-1" style={{ color: "white", fontSize: '80%' }}>
+                                <li className="nav-items">
+                                    <a style={{ color: "white" }} className="nav-link active" aria-current="page" href="/">Início</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a style={{ color: "white" }} className="nav-link" href="/dados">Dados</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a style={{ color: "white" }} className="nav-link" href="/sobre">Sobre</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a style={{ color: "white" }} className="nav-link" href="/creditos">Créditos</a>
+                                </li>
 
-                <li>
-                    <a href="/">Home</a>
-                </li>
-                <li>
-                    <a href="/dados">Dados</a>
-                </li>
-                <li>
-                    <a href="/sobre">Sobre</a>
-                </li>
-                <li>
-                    <a href="/creditos">Créditos</a>
-                </li>
-
-            </ul>
-        </div>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </nav >
+        </>
     )
 }
 
