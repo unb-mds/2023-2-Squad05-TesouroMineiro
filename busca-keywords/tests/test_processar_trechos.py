9ce267c2032052e3bbc25a5d3b616963b64bbe62
CLaudio Henrique <claudio885henrique@gmail.com>
2023-11-26 03:51:49 -0300

    fix test processar trechos

--- busca-keywords/tests/test_processar_trechos.py
+++ busca-keywords/tests/test_processar_trechos.py
@@ -3,27 +3,27 @@ import os
 from export import processar_trechos
 
 
-def test_processar_trechos(tmpdir):
+def test_processar_trechos():
     # Cria um diretório de trechos para teste
-    trechos_dir = os.path.join(str(tmpdir), 'trechos_teste')
-    os.makedirs(trechos_dir)
-
+    pasta_trechos_test = 'busca-keywords/tests/trechos_test'
+    pasta_dados_test = 'busca-keywords/tests/dados_test'
+  
     # Cria um arquivo de trechos para teste
-    arquivo_teste = os.path.join(trechos_dir, 'arquivo_teste.txt')
+    arquivo_teste = os.path.join(pasta_trechos_test, 'arquivo_teste.txt')
     with open(arquivo_teste, 'w') as f:
         f.write("Texto contendo o valor de R$ 1.000,00")
 
     # Testa se a função processar_trechos funciona corretamente
-    palavra_desejada = ("o valor de",)
-    processar_trechos(trechos_dir, palavra_desejada)
+    palavra_desejada = ("o valor de","no valor total de", "no montante de", "com a inclusão de")
+    processar_trechos(pasta_trechos_test, palavra_desejada)
 
     # Verifica se o arquivo JSON foi criado
-    arquivo_json = os.path.join(str(tmpdir), 'busca-keywords/dados', 'arquivo_teste.json')
+    arquivo_json = os.path.join(pasta_dados_test, 'arquivo_teste.json')
     assert os.path.isfile(arquivo_json)
 
     # Verifica o conteúdo do arquivo JSON
     with open(arquivo_json, 'r') as arquivo:
         conteudo = json.load(arquivo)
         assert len(conteudo) == 1
-        assert conteudo[0]['Categoria'] == 'o valor de'
+        assert conteudo[0]['Categoria'] == 'Crédito Suplementar'
         assert conteudo[0]['Soma'] == 1000.0
