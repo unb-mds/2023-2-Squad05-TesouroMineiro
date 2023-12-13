dd462857362cad2e995f7ebca3d167612182e9a3
Elias <101871853+EliasOliver21@users.noreply.github.com>
2023-12-13 01:00:39 -0300

    change in page of data (#113)
    Alteração no título faltante na página Dados.

--- frontend/src/Pages/Dados.js
+++ frontend/src/Pages/Dados.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Pages.css';
+import './Dados.css';
 import Graficos from '../Components/Dados/Graficos'
 
 const Dados = () => {
@@ -8,7 +9,8 @@ const Dados = () => {
 
         <div className="All_Pages w-100 h-100" style={{height: "100vh"}}>
             <div className='h-100'>
-                {/* <h1>Dados</h1> */}
+                <h1 className='Descricao'>Veja os dados!</h1>
+                <p className='titulo'>Selecione o município, a categoria e o período desejados.E confira os valores gastos.</p>
                 <Graficos />
             </div>
         </div>
